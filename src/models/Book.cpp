67cab6d77c1a050b61dae63fa0f3c2caaab7abd0
Prasanna <prasannanile0011@gmail.com>
2026-06-08 06:02:32 +0530

    [day 3.0] Book lifecycle done. Rule of three for Book model

--- src/models/Book.cpp
+++ src/models/Book.cpp
@@ -1,8 +1,40 @@
 #include "models/Book.h"
 
+
+// constructor 
 Book::Book(std::string _bookId, std::string _title, std::string _author, float _price)
-    : bookId{_bookId}, title{_title}, author{_author}, price{_price}, status{BookStatus::AVAILABLE} 
-{}
+    : bookId{_bookId}, title{_title}, author{_author}, price{_price}, status{::BookStatus::AVAILABLE}
+{
+    profileLog = new std::string("Initial Log");
+}
+
+// copy constructor
+Book::Book(const Book& other) 
+    : bookId{other.bookId}, title{other.title}, author{other.author}, price{other.price}, status{::BookStatus::AVAILABLE}
+{
+    profileLog = new std::string(*other.profileLog);
+}
+
+// copy assignment constructor
+Book& Book::operator=(const Book& other) {
+    if(this == &other) return *this;
+
+    delete profileLog;
+    profileLog = new std::string(*other.profileLog);
+
+    bookId = other.bookId;
+    title = other.title;
+    author = other.author;
+    price = other.price;
+    status = ::BookStatus::AVAILABLE;
+
+    return *this;
+}
+
+
+Book::~Book() {
+    delete profileLog;
+}
 
 std::string Book::get_bookId() const { return bookId; }
 std::string Book::get_title() const { return title; }
