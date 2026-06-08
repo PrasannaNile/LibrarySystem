59e1163d1a446039279f58b757bf3683853fd1fe
Prasanna <prasannanile0011@gmail.com>
2026-06-09 01:56:00 +0530

    [Day 5.2] Transaction logic. User issue or returns a book is logged

--- include/models/Transaction.h
+++ include/models/Transaction.h
@@ -3,11 +3,19 @@
 #include <string>
 
 
+enum class TransactionType {ISSUED, RETURNED};
+
 class Transaction {
 private:
     std::string transactionId;
     std::string bookId;
     std::string userId;
+    TransactionType type;
+
 public:
-    Transaction() = default;
+    Transaction(const std::string& transactionId, const std::string& bookId, const std::string& userId, const TransactionType type);
+    std::string get_transactionId() const;
+    std::string get_bookId() const;
+    std::string get_userId() const;
+    TransactionType get_type() const;
 };
