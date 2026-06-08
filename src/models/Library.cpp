8968eb226294458953eefbabfde2624a2c52201b
Prasanna <prasannanile0011@gmail.com>
2026-06-08 21:23:39 +0530

    [Day 5.1] User with user_id issues a book with book_id logic. Transaction issue_book tracked

--- src/models/Library.cpp
+++ src/models/Library.cpp
@@ -11,6 +11,32 @@ void Library::register_user(const User& new_user) {
     users.push_back(new_user);
 }
 
+void Library::issue_book(const std::string bookId, const std::string user_id) {
+    for(auto& book: books) {
+        if(book.get_bookId() == bookId) {
+            BookStatus curr_status = book.get_status();
+            if(curr_status == BookStatus::AVAILABLE) {
+                book.set_status(BookStatus::LOANED);
+
+                std::cout << "Success: Book " << bookId << " successfully issued to user " << user_id << ".\n";
+                return;
+            } else {
+                std::cout << "Error: Book " << bookId << " is currently not available (Status: LOANED/RESERVED/LOST).\n";
+                return;
+            }
+        }
+    }
+
+    std::cout << "Error: Book with ID " << bookId << " does not exist in the library catalog.\n";
+}
+
+void Library::return_book(const std::string bookId, const std::string user_id) {
+    for(auto& book: books) {
+        
+    }
+}
+
+
 void Library::display_book() const {
     for (const auto& book : books) {
         std::cout << "ID: " << book.get_bookId() << " | Title: " << book.get_title() << "\n";
