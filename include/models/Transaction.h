c40d10d48f9295439c8b38fba007b772ee6cb270
Prasanna <prasannanile0011@gmail.com>
2026-06-12 16:34:11 +0530

    [Day 10.1] Modification in Transaction. New Constructor with auto generated ID

--- include/models/Transaction.h
+++ include/models/Transaction.h
@@ -3,7 +3,7 @@
 #include <string>
 
 
-enum class TransactionType {ISSUED, RETURNED};
+enum class TransactionType {ISSUED = 1, RETURNED = 2};
 
 class Transaction {
 private:
@@ -11,11 +11,14 @@ private:
     std::string bookId;
     std::string userId;
     TransactionType type;
+    static int countId;
 
 public:
+    Transaction(const std::string& bookId, const std::string& userId, const TransactionType type);
     Transaction(const std::string& transactionId, const std::string& bookId, const std::string& userId, const TransactionType type);
     std::string get_transactionId() const;
     std::string get_bookId() const;
     std::string get_userId() const;
     TransactionType get_type() const;
+    static void id_incrementor();
 };
