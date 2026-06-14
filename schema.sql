07b4942c5b84b37da76c4499f607e48407a465c6
Prasanna <prasannanile0011@gmail.com>
2026-06-14 10:30:45 +0530

    [Day 12.1] Books and Transactions schema design complete. LibrarySystem db have User, Books, Transaction relation with foreign key constraints

--- schema.sql
+++ schema.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE IF NOT EXISTS LibrarySystem;
 USE LibrarySystem;
 
-
 # ******************* User Schema *******************
 CREATE TABLE IF NOT EXISTS User (
 	userId VARCHAR(50) PRIMARY KEY,
@@ -10,3 +9,28 @@ CREATE TABLE IF NOT EXISTS User (
     role ENUM("ADMIN", "STUDENT") NOT NULL
 );
 # ******************* User Schema ******************* 
+
+
+# ******************* Book Schema *******************
+CREATE TABLE IF NOT EXISTS Book (
+	bookId VARCHAR(50) PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    status ENUM("AVAILABLE", "LOANED", "RESERVED", "LOST") NOT NULL DEFAULT "AVAILABLE"
+);
+# ******************* Book Schema *******************
+
+
+# ******************* Transaction Schema *******************
+CREATE TABLE IF NOT EXISTS Transaction (
+	transactionId VARCHAR(50) PRIMARY KEY,
+    bookId VARCHAR(50) NOT NULL,
+    userId VARCHAR(50) NOT NULL,
+    type ENUM("ISSUED", "RETURNED") NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (bookId) REFERENCES Book(bookId),
+    FOREIGN KEY (userId) REFERENCES User(userId)
+); 
+# ******************* Transaction Schema *******************
+
+SHOW TABLES;
