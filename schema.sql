aad909e872020f4c3ac244e6bd80ba0417d4df26
Prasanna <prasannanile0011@gmail.com>
2026-06-14 10:09:03 +0530

    [Day 12] schema.sql init. LibrarySystem database create. User relation schema design. mysql is in env variables

--- schema.sql
+++ schema.sql
@@ -1,1 +1,12 @@
-ECHO is on.
+CREATE DATABASE IF NOT EXISTS LibrarySystem;
+USE LibrarySystem;
+
+
+# ******************* User Schema *******************
+CREATE TABLE IF NOT EXISTS User (
+	userId VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    role ENUM("ADMIN", "STUDENT") NOT NULL
+);
+# ******************* User Schema ******************* 
